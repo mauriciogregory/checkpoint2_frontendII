c9763c475e91547f99ec262997e269c43ba27a22
mauriciogregory <mauricio.gre@gmail.com>
2021-12-03 08:06:51 -0300

    API card add and Card Layout

--- js/app.js
+++ js/app.js
@@ -1,25 +1,58 @@
-let ul = document.querySelector('ul');
-// let conteudoDOLi = document.createTextNode("Novo item");
-let conteudoDOLi = document.createTextNode("Novo Item")
-let li = document.createElement('li');
-let inputDataCriacao = document.querySelector("#datacriacao");
-let inputDataLimite = document.querySelector("#datalimite");
-let br = document.createElement("br");
 
-let btn = document.querySelector("#btn");
 
-function criarLista(evento){
+btn.addEventListener('click', function criarLista(evento) {
     evento.preventDefault();
 
-    li.innerHTML = "Data de criação: " + inputDataCriacao.value + "<br>" + "Data Limite: " +  inputDataLimite.value;
-    ul.classList.add('li');
-    li.appendChild(conteudoDOLi);
-    document.getElementById("lista").appendChild(li);
-}
+    const btnExcluir = document.createElement('button');
+const checkBOX = document.createElement('input');
+const texto_check = document.createElement('p');
+let ul = document.querySelector('ul');
+// let conteudoDOLi = document.createTextNode("Novo Item")
+let li = document.createElement('li');
+// let inputDataCriacao = document.querySelector("#datacriacao");
+let inputDataLimite = document.querySelector("#datalimite");
+let descricao = document.querySelector("#descricao");
+let br = document.createElement("br");
+let btn = document.querySelector("#btn");
 
-var listaElementos =document.getElementById("lista");
+const agora = new Date;
+console.log(agora);
+// inputDataCriacao.innerHTML = agora.getDay;
+dia = agora.getDay() - 2;
+mes = agora.getMonth() + 1;
+ano = agora.getFullYear();
+
+
+
+var inputDataCriacao = document.querySelector('input[type="date"]');
+inputDataCriacao.value = (ano + "-" + mes + "-0" + dia);
+
+    li.innerHTML = "Data de criação: " + inputDataCriacao.value + "<br>" + "Data Limite: " +
+        inputDataLimite.value + "<br>" + descricao.value;
+    li.classList.add('card');
+
+    // botão excluir
+    li.appendChild(btnExcluir);
+    btnExcluir.setAttribute('type', 'submit');
+    btnExcluir.setAttribute('value', 'Excluir')
+    btnExcluir.innerHTML = "Excluir";
+    btnExcluir.classList.add('btnExcluir')
+
+    // check box
+    li.appendChild(checkBOX);
+    checkBOX.setAttribute('type', 'checkbox');
+    texto_check.innerHTML = 'Concluído!'
+    li.appendChild(texto_check);
+
+    // li.appendChild(conteudoDOLi);
+    ul.appendChild(li);
+
+
+});
+// var listaElementos = document.getElementById("lista");
 // listaElementos.removeChild(listaElementos.lastElementChild);
 
-btn.addEventListener('click', criarLista);
+
+
 
 
