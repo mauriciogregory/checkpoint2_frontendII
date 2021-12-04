11cdc014f6657f577ab8d616f9ba962de2d657e9
Mauricio <33768118+mauriciogregory@users.noreply.github.com>
2021-12-03 23:21:38 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-const checkbox1 = document.getElementById ('checkbox');
+const checkbox1 = document.getElementById('checkbox');
 
 checkbox1.addEventListener('change', () => {
-    //mudar o tema do site
-document.body.classList.toggle('dark');
+  //mudar o tema do site
+  document.body.classList.toggle('dark');
 });
 
 btn.addEventListener('click', function criarLista(evento) {
@@ -36,50 +36,85 @@ btn.addEventListener('click', function criarLista(evento) {
   let mesTermino = validaData[1];
   let diaTermino = validaData[2];
 
-  console.log(mes);
-  if((anoTermino >= ano) && (mesTermino >= mes) && (diaTermino >= dia)){
-      // comparando string textarea validando
-  if (descricao.value.length <= 10) {
-    alert("O número de caracteres precisa ser maior que 10!")
-  } else {
-    li.innerHTML = "Data de criação: " + inputDataCriacao.value + "<br>" + "Data Limite: " +
-    inputDataLimite.value + "<br>" + descricao.value;
-    li.classList.add('card');
-    li.setAttribute('id', 'card1')
+  if ((anoTermino == ano) && ((mesTermino >= mes) && (diaTermino >= dia)) ) {
+    // comparando string textarea validando
+    if (descricao.value.length <= 10) {
+      alert("O número de caracteres precisa ser maior que 10!")
+    } else {
+      li.innerHTML = "Data de criação: " + inputDataCriacao.value + "<br>" + "Data Limite: " +
+        inputDataLimite.value + "<br>" + descricao.value;
+      li.classList.add('card');
+      li.setAttribute('id', 'card1')
 
-    // botão excluir
-    li.appendChild(btnExcluir);
-    btnExcluir.setAttribute('type', 'submit');
-    btnExcluir.setAttribute('value', 'Excluir')
-    btnExcluir.innerHTML = "Excluir";
-    btnExcluir.classList.add('btnExcluir')
+      // botão excluir
+      li.appendChild(btnExcluir);
+      btnExcluir.setAttribute('type', 'submit');
+      btnExcluir.setAttribute('value', 'Excluir')
+      btnExcluir.innerHTML = "Excluir";
+      btnExcluir.classList.add('btnExcluir')
 
-    // checkbox
-    li.appendChild(checkBOX);
-    checkBOX.setAttribute('type', 'checkbox');
-    checkBOX.setAttribute('id', 'checkbox');
-    checkBOX.setAttribute('unchecked', 'false');
-    texto_check.innerHTML = 'Concluído!'
-    li.appendChild(texto_check);
-    ul.appendChild(li);
+      // checkbox
+      li.appendChild(checkBOX);
+      checkBOX.setAttribute('type', 'checkbox');
+      checkBOX.setAttribute('id', 'checkbox');
+      checkBOX.setAttribute('unchecked', 'false');
+      texto_check.innerHTML = 'Concluído!'
+      li.appendChild(texto_check);
+      ul.appendChild(li);
 
+      checkBOX.addEventListener('change', () => {
 
-  checkBOX.addEventListener('click', () => {
+        if (document.getElementById('checkbox').checked = true) {
+          li.classList.add('card-done');
+        }
+        else if (document.getElementById('checkbox').unchecked = true) {
 
-    if (document.getElementById('checkbox').checked) {
-      li.classList.add('card-done');
+          li.className.remove('card-done');
+        }
+      })
     }
-    else if (document.getElementById('checkbox').unchecked) {
+  } else 
+  if (anoTermino > ano){
+    // comparando string textarea validando
+    if (descricao.value.length <= 10) {
+      alert("O número de caracteres precisa ser maior que 10!")
+    } else {
+      li.innerHTML = "Data de criação: " + inputDataCriacao.value + "<br>" + "Data Limite: " +
+        inputDataLimite.value + "<br>" + descricao.value;
+      li.classList.add('card');
+      li.setAttribute('id', 'card1')
 
-      li.className.remove('card-done');
+      // botão excluir
+      li.appendChild(btnExcluir);
+      btnExcluir.setAttribute('type', 'submit');
+      btnExcluir.setAttribute('value', 'Excluir')
+      btnExcluir.innerHTML = "Excluir";
+      btnExcluir.classList.add('btnExcluir')
+
+      // checkbox
+      li.appendChild(checkBOX);
+      checkBOX.setAttribute('type', 'checkbox');
+      checkBOX.setAttribute('id', 'checkbox');
+      checkBOX.setAttribute('unchecked', 'false');
+      texto_check.innerHTML = 'Concluído!'
+      li.appendChild(texto_check);
+      ul.appendChild(li);
+
+      checkBOX.addEventListener('change', () => {
+
+        if (document.getElementById('checkbox').checked = true) {
+          li.classList.add('card-done');
+        }
+        else if (document.getElementById('checkbox').unchecked = true) {
+
+          li.className.remove('card-done');
+        }
+      })
     }
-  })
   }
-        
-  } else {
+  else {
     alert("Escolha outra data!")
   }
-
 
 
   btnExcluir.addEventListener('click',
